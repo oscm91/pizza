5d5ef8e23913d01b3443b3a6780f007f399d86d9
Oscar Armando Mora <oscar.mora@ip-192-168-1-101.us-east-2.compute.internal>
2021-10-28 20:39:09 -0500

    Update nx

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,14 @@
-const path = require('path');
-const deps = require("./package.json").dependencies;
-const { ModuleFederationPlugin } = require('webpack').container;
 const nrwlConfig = require('@nrwl/react/plugins/webpack.js');
 
 module.exports = (config, context) => {
   nrwlConfig(config);
   return {
     ...config,
+    mode: "development",
+    optimization: {
+      ...config.optimization,
+      runtimeChunk: false
+    },
     module: {
       ...config.module,
       rules: [...config.module.rules, {
@@ -14,29 +16,6 @@ module.exports = (config, context) => {
         use: { loader: "worker-loader" },
       }],
     },
-    plugins: [...config.plugins,
-    new ModuleFederationPlugin({
-      name: "app_enterprise",
-      filename: "remoteEntry.js",
-      exposes: {
-        "./store": path.resolve(__dirname, "./libs/store/src/index"),
-        "./logo": path.resolve(__dirname, "./libs/ui/src/lib/logo/index")
-      },
-      shared: [
-        {
-          ...deps,
-          react: {
-            // eager: true,
-            singleton: true,
-            requiredVersion: deps.react,
-          },
-          "react-dom": {
-            // eager: true,
-            singleton: true,
-            requiredVersion: deps["react-dom"],
-          },
-        },
-      ],
-    })],
+    plugins: [...config.plugins],
   };
 };
